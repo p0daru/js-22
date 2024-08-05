81bf79de2c224c30f7232dece20f512234489841
p0daru <102858022+p0daru@users.noreply.github.com>
2024-08-05 14:14:10 +0300

    module-3 ls-5-6

--- js/01-spread.js
+++ js/01-spread.js
@@ -3,71 +3,194 @@
  * - Array.prototype.concat() и аналог через spread
  */
 
+// метод concat - робить склейку масивів
 // const numbers = [1, 2, 3].concat([4, 5, 6], [7, 8, 9]);
 
-const numbers = [
-  1000,
-  ...[1, 2, 3],
-  5000,
-  ...[4, 5, 6],
-  7000,
-  ...[7, 8, 9],
-  9000,
-];
+// console.log(numbers); // копія масиву [1, 2, 3]. оригінальний масив [1, 2, 3] НІЯК НЕ ЗМІНИВСЯ!!
 
-// console.log(numbers);
+// spread
+// const numbers = [
+//   1000,
+//   ...[1, 2, 3],
+//   5000,
+//   ...[4, 5, 6],
+//   7000,
+//   ...[7, 8, 9],
+//   9000,
+// ];
 
-/*
- * Поиск самой маленькой или большой температуры (числа)
- */
-const temps = [18, 14, 12, 21, 17, 29, 24];
+// // console.log(numbers);
 
-// console.log(Math.max(1, 4, 17, 5, 6));
+// /*
+//  * Поиск самой маленькой или большой температуры (числа)
+//  */
+// const temps = [18, 14, 12, 21, 17, 29, 24];
+
+// console.log(Math.max(1, 4, 17, 10));
+// console.log(Math.max(...temps));
 // console.log(Math.min(...temps));
-// console.log(temps);
 
 // const a = [{ x: 1 }, { y: 2 }, { z: 3 }];
-
 // const b = [...a];
 
 // console.log('a: ', a);
 // console.log('b: ', b);
 
-// console.log(a[0] === b[0]);
-// console.log(a === b);
+// console.log(a[0] === b[0]); // посилання на ОДИН і ТОЙ САМИЙ об`єкт
+// console.log(a === b); // при цьому масиви РІЗНІ
 
 // a[0].x = 1000;
 
-// console.log('a: ', a);
-// console.log('b: ', b);
+// console.log(a);
+// console.log(b);
 
-/*
- * Сшиваем несколько массивов в один через concat() и spread
+// const x = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ];
+
+// const y = [...x];
+
+// console.log(x);
+// console.log(y);
+
+// x[0].push(3);
+// console.log(x);
+// console.log(y);
+
+// /*
+//  * Сшиваем несколько массивов в один через concat() и spread
+//  */
+// const lastWeekTemps = [1, 2, 3];
+// const currentTemps = [4, 5, 6];
+// const nextWeekTemps = [7, 8, 9];
+
+// const allTemps = [...lastWeekTemps, ...currentTemps, ...nextWeekTemps];
+// console.log(allTemps);
+// console.log(lastWeekTemps);
+
+// АБО
+// const allTemps1 = lastWeekTemps.concat(currentTemps, nextWeekTemps);
+// console.log(allTemps1);
+// console.log(lastWeekTemps);
+
+/**
+ * ПРАВИЛО: якщо елемент якогось із масивів - складний тип
+ * При розпиленні масивів, що складаються зі складних типів, у новий масив
+ * і зміні елементів цього нового масиву, змінюються і оригінальні масиви.
+ * Бо при РОЗПИЛЕННІ складних типів буде ПОСИЛАННЯ, а НЕ КОПІЯ
+ * При CONCAT() теж саме!!!!
  */
-const lastWeekTemps = [1, 2, 3];
+const lastWeekTemps = [[1], 2, 3]; // тут перший елемент - це масив, тобто складний тип
 const currentTemps = [4, 5, 6];
 const nextWeekTemps = [7, 8, 9];
 
-const allTemps = [...lastWeekTemps, ...currentTemps, ...nextWeekTemps];
+// const allTemps = [...lastWeekTemps, ...currentTemps, ...nextWeekTemps]; // розпиляємо
 // console.log(allTemps);
+// console.log(lastWeekTemps);
 
-/*
- * Распыление объектов
- * - Object.prototype.assign() и spread
- */
+// allTemps.push(5);
+// console.log(allTemps);
+// console.log(lastWeekTemps); // тут змін ніяких, бо ми змінюємо масив allTemps, а не lastWeekTemps
+
+// // ОДНАК!! Якщо ми змінимо перший елемент масиву allTemps, який є ПОСИЛАННЯМ на масив lastWeekTemps (при розпиленні масиву зі складних типів, буде посилання замість копії)
+// allTemps[0].push(15);
+// console.log(allTemps);
+// console.log(lastWeekTemps); // спостерігаємо ЗМІНИ
+
+// АБО
+// const allTemps1 = lastWeekTemps.concat(currentTemps, nextWeekTemps);
+// console.log(allTemps1);
+// console.log(lastWeekTemps); // залишається як був
+
+// allTemps1[0].push(2);
+// console.log(allTemps1);
+// console.log(lastWeekTemps); // змінюється
+
+// /*
+//  * Распыление объектов
+//  * - Object.prototype.assign() и spread
+//  */
 const a = { x: 1, y: 2 };
 const b = { x: 0, z: 3 };
 
-// const c = Object.assign({name: 'Mango' }, a, b);
+// const c = Object.assign({}, a, b); // розпиляє зліва-направо і перезаписує старі значення ключів, які вже є
+
+// console.log(c);
 
 const c = {
   ...a,
-  x: 10,
+  x: 15,
   ...b,
   y: 20,
 };
 
-// console.log(c);
+console.log(c);
+
+/// ПРИКЛАДИ
+// const fav23 = { song1: 'Home', song2: 'JPEG RAW', x: 15 };
+// const fav24 = { song1: 'Rock', song3: 'Mercy' };
+
+// const allFavs = {
+//   song1: 'Hello',
+//   ...fav23,
+//   song2: 'Hahha',
+//   ...fav24,
+// };
+
+// console.log(allFavs);
+
+// const monSchedule = { l1: 'Math', l2: 'History', l3: 'Chemistry' };
+// const frSchedule = { l1: 'Physics', l5: 'Art', l6: 'Chemistry', l2: 'History' };
+
+// const schedule = {
+//   l1: 'Foreign literature',
+//   ...monSchedule,
+//   l2: 'English',
+//   l4: 'Ukrainian',
+//   ...frSchedule,
+//   l7: 'German',
+// };
+
+// console.log(schedule);
+
+// {
+//   l1: 'Physics',
+//   l2: 'History'
+//   l3: 'Chemistry',
+//   l4: 'Ukrainian',
+//   l5: 'Art',
+//   l6: 'Chemistry',
+//   l7: 'German',
+// }
+
+// const europe = { country1: 'Belgium', country2: 'France', country3: 'Sweden' };
+// const northAmerica = {
+//   country1: 'USA',
+//   country2: 'Mexico',
+//   country4: 'Greenland',
+// };
+
+// const countries = {
+//   ...europe,
+//   country1: 'Ukraine',
+//   country5: 'UK',
+//   ...northAmerica,
+//   country3: 'Australia',
+//   country7: 'Norway',
+// };
+
+// console.log(countries);
+
+// const res = {
+//   country1: 'USA',
+//   country2: 'Mexico',
+//   country3: 'Australia',
+//   country4: 'Greenland',
+//   country5: 'UK',
+//   country7: 'Norway',
+// };
 
 const defaultSettings = {
   theme: 'light',
--- js/02-destructuring.js
+++ js/02-destructuring.js
@@ -11,15 +11,32 @@ const playlist = {
   trackCount: 3,
 };
 
+// якщо це примітив, то при деструктуризації, створюється КОПІЯ
+// якщо складний тип, то – ПОСИЛАННЯ
 // const {
-//   rating,
-//   tracks,
+//   rating = 1, // дефолтне значення
+//   tracks = [],
 //   name,
-//   trackCount: numberOfTracks = 0,
+//   trackCount: numberOfTracks = 0, // зліва - ім`я властивості, справа - ім`я локальної змінної
 //   author = 'user',
 // } = playlist;
+// console.log(rating); // 5 (щоб спрацював код, треба LET)
 
-// console.log(numberOfTracks);
+// playlist.rating = 7;
+// console.log(playlist); // 7;
+// console.log(rating); // 5
+
+// rating = 10;
+// console.log(rating); // 10
+// console.log(playlist); // playlist.rating = 7, тобто ЗМІН НЕМАЄ, бо ЦЕ КОПІЯ
+
+// tracks.push('трек-4');
+// console.log(tracks);
+// console.log(playlist.tracks); // змінився
+
+/// властивості немає в об`єкті => undefined або default-значення
+// 99% властивостей БЕЗ ДЕФОЛТНИХ ЗНАЧЕНЬ
+// console.log(author);
 
 /*
  * Глубокая деструктуризация
@@ -34,47 +51,78 @@ const playlist = {
 //     followers: 5603,
 //     views: 4827,
 //     likes: 1308,
+//     a: {
+//       b: 1,
+//       c: 1,
+//       d: 1,
+//     },
 //   },
 // };
 
+// const { name, tag, location, avatar, stats } = profile;
+
+// const { followers, views, likes } = stats;
+
 // const {
-//   avatar,
 //   name,
 //   tag,
 //   location,
-//   stats: { followers, views, likes },
+//   avatar,
+//   stats: {
+//     followers: myFollowers = 5,
+//     views,
+//     likes,
+//     a: { b, c, d }, // деструктуризуй стати
+//   },
 // } = profile;
 
-// console.log(name, tag, location, avatar, followers, views, likes);
+// console.log(name, tag, location, avatar, myFollowers, views, likes, b, c, d);
+
+//// Можна окремо деструктуризувати
+// const { name } = profile;
+// const { tag } = profile;
+
+// console.log(name, tag);
+
+// const { name: name1 } = profile;
+// const { name: name2 } = profile;
+
+// console.log(name1, name2);
 
 /*
  * Деструктуризация массивов
  */
 
-const rgb = [255, 100, 80];
+// const rgb = [255, 100, 80];
 
-const [red, green, blue] = rgb;
-
+// const [red, green, blue] = rgb; // в якому порядку об`явили змінні, в такому і записались в них значення зліва-направо
 // console.log(red, green, blue);
 
-const authors = {
-  kiwi: 4,
-  poly: 7,
-  ajax: 9,
-  mango: 6,
-};
+// // якщо треба пропустити властивість
+// const [red1, , blue1] = rgb;
+// console.log(red1, blue1);
 
-const entries = Object.entries(authors);
+// const authors = {
+//   kiwi: 4,
+//   poly: 7,
+//   ajax: 9,
+//   mango: 6,
+// };
 
+// const ratings = Object.values(authors);
+// console.log(Math.max(...ratings));
+
+//// ENTRIES
+// const entries = Object.entries(authors);
 // console.log(entries);
 
 // for (const [name, rating] of entries) {
-//   // ур2
-//   // const [name, rating] = entry;
-
-//   // ур1
+//   // lv1
 //   // const name = entry[0];
 //   // const rating = entry[1];
+
+//   // lv2
+//   // const [name, rating] = entry;
 
 //   console.log(name, rating);
 // }
@@ -105,24 +153,42 @@ const entries = Object.entries(authors);
  * - деструктуризация параметра-обьекта в подписи функции
  * - rest при деструктуризации в подписи
  */
+// const profile = {
+//   name: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: 'Ocho Rios, Jamaica',
+//   avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
 
-const showProfileInfo = function (userProfile) {
-  const { name, tag, location, ...restProps } = userProfile;
+// const showProfileInfo = function (userProfile) {
+//   const {
+//     name,
+//     tag,
+//     location,
+//     avatar,
+//     stats: { followers, views, likes },
+//   } = userProfile;
 
-  // console.log(name, tag, location, avatar, followers, views, likes);
-  console.log(restProps);
-};
+//   console.log(name, tag, location, avatar, followers, views, likes);
+// };
 
-const profile = {
-  name: 'Jacques Gluke',
-  tag: 'jgluke',
-  location: 'Ocho Rios, Jamaica',
-  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+// const showProfileInfo = function ({
+//   name,
+//   tag,
+//   location,
+//   avatar,
+//   stats: { followers, views, likes },
+// }) {
+//   console.log(name, tag, location, avatar, followers, views, likes);
+// };
 
-showProfileInfo(profile);
+// const showProfileInfo = function ({ name, tag, location, ...restProps }) {
+//   console.log(name, tag, location, restProps);
+// };
+
+// showProfileInfo(profile);
